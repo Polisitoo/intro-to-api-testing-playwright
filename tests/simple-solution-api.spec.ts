5fa4f76ffc2d35acdb0a0ffc80fe147ee8cb0a9a
popoc <popochanel999@gmail.com>
2025-07-11 12:50:50 +0300

    fix: apply Prettier formatting to pass CI

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -12,7 +12,6 @@ test('get order with correct id should receive code 200', async ({ request }) =>
   expect(response.status()).toBe(200)
 })
 
-
 test('post order with correct data should receive code 201', async ({ request }) => {
   // prepare request body
   const requestBody = {
@@ -32,7 +31,6 @@ test('post order with correct data should receive code 201', async ({ request })
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
-
 
 test('get order with orderID 0 should receive code 400', async ({ request }) => {
   // Build and send a GET request to the server
